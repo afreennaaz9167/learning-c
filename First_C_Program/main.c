a294ecd5b09c50f9ddfb238aedf52fdf28b99923
afreennaaz9167 <afreennaaz9167@gmail.com>
2024-05-12 13:50:10 +0530

    printing playing to terminal

---
+++ First_C_Program/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+void main()
+{
+    printf("playing ");
+}
+
+// Language Translater also known as 'Compiler' for c
+// Well lnown compiler's are GCC,CLANG,MSVC etc
+// GCC and clang canbe downloaded from winlibs.com
